719e8d120a6bbbcc8de54eb36d7aa6fcbb6ddda4
geonwoo92 <cnu201102054@gmail.com>
2024-04-29 12:00:50 +0900

    24-04-29 Schedule Update

---
+++ soccer-server/backend/src/main/java/com/erichgamma/api/schedule/controller/ScheduleController.java
@@ -0,0 +1,21 @@
+package com.erichgamma.api.schedule.controller;
+
+import com.erichgamma.api.schedule.Service.ScheduleService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "400", description = "Invalid ID supplied"),
+        @ApiResponse(responseCode = "404", description = "Customer not found")})
+@RequiredArgsConstructor
+@Slf4j
+public class ScheduleController {
+    private final ScheduleService scheduleService;
+}
