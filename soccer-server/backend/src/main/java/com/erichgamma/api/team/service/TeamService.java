a6fa3971d6bb2cb56030d182404c31196730bebb
6whistle <camjun0506@icloud.com>
2024-04-29 11:22:23 +0900

    ADD TEAM 2024-04-29 jhl

---
+++ soccer-server/backend/src/main/java/com/erichgamma/api/team/service/TeamService.java
@@ -0,0 +1,27 @@
+package com.erichgamma.api.team.service;
+
+import com.erichgamma.api.common.command.CommandService;
+import com.erichgamma.api.common.query.QueryService;
+import com.erichgamma.api.team.model.Team;
+import com.erichgamma.api.team.model.TeamDto;
+
+public interface TeamService extends CommandService<TeamDto>, QueryService<TeamDto> {
+    default TeamDto EntityToDto(Team entity){
+        return TeamDto.builder()
+        .id(entity.getId())
+        .teamId(entity.getTeamId())
+        .regionName(entity.getRegionName())
+        .teamName(entity.getTeamName())
+        .origYyyy(entity.getOrigYyyy())
+        .stadiumId(entity.getStadiumId())
+        .zipCode1(entity.getZipCode1())
+        .zipCode2(entity.getZipCode2())
+        .address(entity.getAddress())
+        .ddd(entity.getDdd())
+        .tel(entity.getTel())
+        .fax(entity.getFax())
+        .homepage(entity.getHomepage())
+        .owner(entity.getOwner())
+        .build();
+    }
+}
---
+++ soccer-server/backend/src/main/java/com/erichgamma/api/team/service/TeamServiceImpl.java
@@ -0,0 +1,72 @@
+package com.erichgamma.api.team.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.erichgamma.api.common.component.MessengerVo;
+import com.erichgamma.api.team.model.TeamDto;
+import com.erichgamma.api.team.repository.TeamRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TeamServiceImpl implements TeamService {
+    private final TeamRepository teamRepository;
+
+    @Override
+    public MessengerVo save(TeamDto t) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public MessengerVo modify(TeamDto t) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'modify'");
+    }
+
+    @Override
+    public MessengerVo insertMany() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insertMany'");
+    }
+
+    @Override
+    public MessengerVo deleteById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+
+    @Override
+    public MessengerVo deleteAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAll'");
+    }
+
+    @Override
+    public List<TeamDto> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Optional<TeamDto> findById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public Long count() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'count'");
+    }
+
+    @Override
+    public Boolean existsById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'existsById'");
+    }
+}
